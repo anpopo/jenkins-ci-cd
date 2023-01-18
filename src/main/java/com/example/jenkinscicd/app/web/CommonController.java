bf632ccc62ec26c490dc227105c34f3701317965
ansehyeong <dkstpgud@gmail.com>
2023-01-18 17:36:17 +0900

    feat : jenkins Poll SCM 확인을 위한 코드 수정

---
+++ src/main/java/com/example/jenkinscicd/app/web/CommonController.java
@@ -0,0 +1,15 @@
+package com.example.jenkinscicd.app.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/common")
+@RestController
+public class CommonController {
+
+    @GetMapping
+    public String common() {
+        return "hi~";
+    }
+}
